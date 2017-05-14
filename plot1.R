7f3bb4c3b45da21fe84ec4f247c5ff43e75f3314
Kimtai Kiminy <sammykiminy@yahoo.com>
2017-05-14 08:56:12 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,18 @@
+##Plot1.R
+##Kimtai Kiminy
+##May 12 2017
+
+#chacking the dataset
 dataFile <- "./data/household_power_consumption.txt"
+
+#reading the dataset
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #str(subSetData)
 globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
+
+#ploting the histogram to to the png files
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+#closing and saving the png graphic
 dev.off()
